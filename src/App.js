e61f42599b13719e1a0765a975528aac6fe35e15
dhanrajchaurasia <mnnitdhanraj@gmail.com>
2021-11-20 15:37:05 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import style from "./App.css";
 var error = false;
+var opr = false;
+var num = false;
 
 const App = () => {
     const [given, result] = useState("");
+    const work = (e) => {
+        if (e.target.name === '-') {
+            clicked(e);
+        }
+        if (num === true) {
+            clicked(e);
+            opr = true;
+        }
+    }
+    const work2 = (e) => {
+        num = true;
+        opr = false;
+        clicked(e);
+    }
     const clicked = (e) => {
         if (error === true) {
             error = false;
             result(e.target.name);
         }
-        else
+        else {
+            if (opr === true) {
+                backspace();
+            }
             result(given.concat(e.target.name));
+            opr = true;
+        }
     }
     const clear = () => {
         result("");
@@ -37,6 +58,8 @@ const App = () => {
     }
     return (
         <>
+            <aside>
+            </aside>
             <div className="container">
                 {/* <h1>Calculator</h1> */}
                 <form>
@@ -45,26 +68,26 @@ const App = () => {
                 <div className="keypad">
                     <button onClick={clear} id="clear">Clear</button>
                     <button onClick={backspace} id="backspace">C</button>
-                    <button className="side" name="/" onClick={clicked}>&divide;</button>
-                    <button name="7" onClick={clicked}>7</button>
-                    <button name="8" onClick={clicked}>8</button>
-                    <button name="9" onClick={clicked}>9</button>
-                    <button className="side" name="*" onClick={clicked}>&times;</button>
-                    <button name="4" onClick={clicked}>4</button>
-                    <button name="5" onClick={clicked}>5</button>
-                    <button name="6" onClick={clicked}>6</button>
-                    <button className="side" name="-" onClick={clicked}>&ndash;</button>
-                    <button name="1" onClick={clicked}>1</button>
-                    <button name="2" onClick={clicked}>2</button>
-                    <button name="3" onClick={clicked}>3</button>
-                    <button className="side" name="+" onClick={clicked}>+</button>
-                    <button name="0" onClick={clicked}>0</button>
-                    <button name="." onClick={clicked}>.</button>
+                    <button className="side" name="/" onClick={work}>&divide;</button>
+                    <button name="7" onClick={work2}>7</button>
+                    <button name="8" onClick={work2}>8</button>
+                    <button name="9" onClick={work2}>9</button>
+                    <button className="side" name="*" onClick={work}>&times;</button>
+                    <button name="4" onClick={work2}>4</button>
+                    <button name="5" onClick={work2}>5</button>
+                    <button name="6" onClick={work2}>6</button>
+                    <button className="side" name="-" onClick={work}>&ndash;</button>
+                    <button name="1" onClick={work2}>1</button>
+                    <button name="2" onClick={work2}>2</button>
+                    <button name="3" onClick={work2}>3</button>
+                    <button className="side" name="+" onClick={work}>+</button>
+                    <button name="0" onClick={work2}>0</button>
+                    <button name="." onClick={work2}>.</button>
                     <button onClick={clicked} onClick={calculate} id="result">=</button>
                 </div>
             </div>
         </>
     );
-}
+}       
 
 export default App;
